12405bc8054e73abf77c26d1ffdc25f0f799d94e
Shawn Chang <ebola777@yahoo.com.tw>
2018-12-16 19:57:54 +0800

    cleanup

--- Assignment4/src/skybox_shader.cpp
+++ Assignment4/src/skybox_shader.cpp
@@ -33,13 +33,13 @@ void shader::SkyboxShader::InitUniformBlocks() {
   as::UniformManager &uniform_manager = gl_managers_->GetUniformManager();
   // Get names
   const std::string &program_name = GetProgramName();
-  const std::string &global_mvp_buffer_name = GetGlobalTransBufferName();
-  const std::string &global_mvp_uniform_block_name =
+  const std::string &global_trans_buffer_name = GetGlobalTransBufferName();
+  const std::string &global_trans_uniform_block_name =
       GetGlobalTransUniformBlockName();
-  const std::string &global_mvp_binding_name = global_mvp_buffer_name;
+  const std::string &global_trans_binding_name = global_trans_buffer_name;
   // Bind the uniform block to the the buffer
   uniform_manager.AssignUniformBlockToBindingPoint(
-      program_name, global_mvp_uniform_block_name, global_mvp_binding_name);
+      program_name, global_trans_uniform_block_name, global_trans_binding_name);
 }
 
 void shader::SkyboxShader::InitTextures() {
