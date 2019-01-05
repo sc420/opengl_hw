c676fe35c2b51bdd8378bc1f35f0695c833e1a7f
Shawn Chang <ebola777@yahoo.com.tw>
2019-01-05 21:49:30 +0800

    cleanup

--- Final/src/main.cpp
+++ Final/src/main.cpp
@@ -88,62 +88,6 @@ enum ModeMenuItems { kModeComparison, kModeNavigation };
 enum NormalHeightMenuItems { kNormalHeightOn, kNormalHeightOff };
 enum TimerMenuItems { kTimerStart, kTimerStop };
 
-static bool show_demo_window = false;
-static bool show_another_window = false;
-static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-void my_display_code() {
-  // 1. Show the big demo window (Most of the sample code is in
-  // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
-  // ImGui!).
-  // if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
-
-  // 2. Show a simple window that we create ourselves. We use a Begin/End pair
-  // to created a named window.
-  {
-    static float f = 0.0f;
-    static int counter = 0;
-
-    ImGui::Begin("Hello, world!");  // Create a window called "Hello, world!"
-                                    // and append into it.
-
-    ImGui::Text("This is some useful text.");  // Display some text (you can use
-                                               // a format strings too)
-    ImGui::Checkbox(
-        "Demo Window",
-        &show_demo_window);  // Edit bools storing our window open/close state
-    ImGui::Checkbox("Another Window", &show_another_window);
-
-    ImGui::SliderFloat("float", &f, 0.0f,
-                       1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
-    ImGui::ColorEdit3(
-        "clear color",
-        (float *)&clear_color);  // Edit 3 floats representing a color
-
-    if (ImGui::Button("Button"))  // Buttons return true when clicked (most
-                                  // widgets return true when edited/activated)
-      counter++;
-    ImGui::SameLine();
-    ImGui::Text("counter = %d", counter);
-
-    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-    ImGui::End();
-  }
-
-  // 3. Show another simple window.
-  if (show_another_window) {
-    ImGui::Begin(
-        "Another Window",
-        &show_another_window);  // Pass a pointer to our bool variable (the
-                                // window will have a closing button that will
-                                // clear the bool when clicked)
-    ImGui::Text("Hello from another window!");
-    if (ImGui::Button("Close Me")) show_another_window = false;
-    ImGui::End();
-  }
-}
-
 /*******************************************************************************
  * GL Initializations
  ******************************************************************************/
@@ -219,6 +163,103 @@ void ConfigGL() {
 }
 
 /*******************************************************************************
+ * GUI Handlers
+ ******************************************************************************/
+
+void InitImGui() {
+  // Setup Dear ImGui context
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  // Enable Keyboard/Controls
+  // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  // ImGui::StyleColorsClassic();
+
+  // Setup Platform/Renderer bindings
+  ImGui_ImplFreeGLUT_Init();
+  // ImGui_ImplFreeGLUT_InstallFuncs();
+  ImGui_ImplOpenGL3_Init();
+}
+
+void DestoryImGui() {
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplFreeGLUT_Shutdown();
+  ImGui::DestroyContext();
+}
+
+void UpdateImGui() {
+  static bool show_demo_window = false;
+  static bool show_another_window = false;
+  static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  // 1. Show the big demo window (Most of the sample code is in
+  // ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear
+  // ImGui!).
+  // if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
+
+  // 2. Show a simple window that we create ourselves. We use a Begin/End pair
+  // to created a named window.
+  {
+    static float f = 0.0f;
+    static int counter = 0;
+
+    ImGui::Begin("Hello, world!");  // Create a window called "Hello, world!"
+                                    // and append into it.
+
+    ImGui::Text("This is some useful text.");  // Display some text (you can use
+                                               // a format strings too)
+    ImGui::Checkbox(
+        "Demo Window",
+        &show_demo_window);  // Edit bools storing our window open/close state
+    ImGui::Checkbox("Another Window", &show_another_window);
+
+    ImGui::SliderFloat("float", &f, 0.0f,
+                       1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
+    ImGui::ColorEdit3(
+        "clear color",
+        (float *)&clear_color);  // Edit 3 floats representing a color
+
+    if (ImGui::Button("Button"))  // Buttons return true when clicked (most
+                                  // widgets return true when edited/activated)
+      counter++;
+    ImGui::SameLine();
+    ImGui::Text("counter = %d", counter);
+
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    ImGui::End();
+  }
+
+  // 3. Show another simple window.
+  if (show_another_window) {
+    ImGui::Begin(
+        "Another Window",
+        &show_another_window);  // Pass a pointer to our bool variable (the
+                                // window will have a closing button that will
+                                // clear the bool when clicked)
+    ImGui::Text("Hello from another window!");
+    if (ImGui::Button("Close Me")) show_another_window = false;
+    ImGui::End();
+  }
+}
+
+void DrawImGui() {
+  // Start the Dear ImGui frame
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplFreeGLUT_NewFrame();
+
+  // Update ImGui
+  UpdateImGui();
+
+  // Draw ImGui on default framebuffer
+  ImGui::Render();
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+/*******************************************************************************
  * GL States Updaters
  ******************************************************************************/
 
@@ -278,19 +319,9 @@ void GLUTDisplayCallback() {
   as::ClearDepthBuffer();
   postproc_shader.Draw();
 
-  // Start the Dear ImGui frame
-  ImGui_ImplOpenGL3_NewFrame();
-  ImGui_ImplFreeGLUT_NewFrame();
-
-  my_display_code();
-
   // Draw ImGui on default framebuffer
-  ImGui::Render();
-  ImGuiIO &io = ImGui::GetIO();
-  // glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
-  glUseProgram(0);  // You may want this if using this code in an OpenGL 3+
-  // context where shaders may be bound, but prefer using the GL3+ code.
-  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  scene_shader.UseDefaultFramebuffer();
+  DrawImGui();
 
   // Swap double buffers
   glutSwapBuffers();
@@ -628,31 +659,11 @@ int main(int argc, char *argv[]) {
     InitGLUT(argc, argv);
     as::InitGLEW();
     ConfigGL();
+    InitImGui();
     RegisterGLUTCallbacks();
     CreateGLUTMenus();
-
-    // Setup Dear ImGui context
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    (void)io;
-    // Enable Keyboard/Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    ImGui::StyleColorsClassic();
-
-    // Setup Platform/Renderer bindings
-    ImGui_ImplFreeGLUT_Init();
-    // ImGui_ImplFreeGLUT_InstallFuncs();
-    ImGui_ImplOpenGL3_Init();
-
     EnterGLUTLoop();
-
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplFreeGLUT_Shutdown();
-    ImGui::DestroyContext();
+    DestoryImGui();
   } catch (const std::exception &ex) {
     std::cerr << "Exception: " << ex.what() << std::endl;
     throw;
